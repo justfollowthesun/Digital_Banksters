bc8b249dbc5fe9b8a74f2028fc05745fe389801d
followthesun <anivan9889@gmail.com>
2022-06-11 13:13:31 +0300

    FIAT: add vk parser and face processing algorithm

---
+++ singlepage/src/config.py
@@ -0,0 +1,12 @@
+token = '4742fa094742fa094742fa09d7473e01c1447424742fa0925e15134667cd55970cb83ef'
+client_id = 8190920
+version = '5.65'
+#######
+secret_key = '3bKc4Ks53VGyKYzwyjaZ'
+v = '5.95'
+access_token = 'vk1.a.e9BEemTZ_M-bYs_lFDmJ9XU5-KVkP2sQNhXBDeevFJDk-RK9DKrFkXmMfqR6c-naJK3X-mN3k3pDBokuyDF9WxO_DJJBYrgJnGDCDryrl4O3YPKoM2LqTzC6I_dwxkLMXvYXyuF5t5yJomJT17_xp4TmqYgNJzDlicc3xMdkT2j5k214hsrJdaS33jXrn4WJ'
+
+'''
+Access token can be taken via request in 
+https://oauth.vk.com/authorize?client_id=8190920&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=SCOPE&response_type=token&v=5.65
+'''
